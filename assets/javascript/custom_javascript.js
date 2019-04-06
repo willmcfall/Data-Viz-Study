2a64ef7003046a0cad1ec47fce91a507da4f1d89
William McFall <wmcfall2@gmail.com>
2019-04-06 12:20:07 -0400

    second commit

--- assets/javascript/custom_javascript.js
+++ assets/javascript/custom_javascript.js
@@ -7,74 +7,71 @@ $(document).ready(function () {
 
     // Section to create bar chart container and bars
 
+    // Creates a variable holding the values to be vizualized
     var dataset = [80, 100, 56, 120, 180, 30, 40, 120, 160];
 
-    var svgWidth = 500, svgHeight = 300, barPadding = 5;
+    // Creates a number of variables which define the dimensions of the svg container
+    var containerWidth = +d3.select('.col-12').style('width').slice(0, -2)
+    var svgWidth = containerWidth * .80;
+    var svgHeight = 300;
+    var barPadding = 5;
     var barWidth = (svgWidth / dataset.length);
 
-    var svg = d3.select('.bar-chart')
-        .attr("width", svgWidth)
-        .attr("height", svgHeight);
-
-    var xScale = d3.scaleLinear()
-        .domain([0, d3.max(dataset)])
+    // Creates a variables that allows for scaling of the x axes of the chart elements to fit the container
+    var x = d3.scaleLinear()
+        .domain([0, dataset.length])
         .range([0, svgWidth]);
 
-    var yScale = d3.scaleLinear()
+    var y = d3.scaleLinear()
         .domain([0, d3.max(dataset)])
         .range([svgHeight, 0]);
 
+    // Select svg element and add attributes to the element
+    var svg = d3.select("svg")
+        .attr("width", svgWidth)
+        .attr("height", svgHeight)
+        .attr("class", "bar-chart");
 
-    var barChart = svg.selectAll("rect")
+    // Create new rect elements within the svg elemetn and add atributes
+    svg.selectAll("rect")
         .data(dataset)
-        .enter()
-        .append("rect")
-        .attr("y", function (d) {
-            return svgHeight - yScale(d)
-        })
-        .attr("height", function (d) {
-            return yScale(d);
-        })
+        .enter().append("rect")
+        .attr("class", "bar")
         .attr("width", barWidth - barPadding)
-        .attr("transform", function (d, i) {
-            var translate = [barWidth * i, 0];
-            return "translate(" + translate + ")";
-        });
+        .attr("height", function (d) { return svgHeight - y(d); })
+        .attr("x", function (d, i) { return x(i); })
+        .attr("y", function (d) { return y(d) - 20;});
 
-    // Section to create labels for bars
-
-    var text = svg.selectAll("text")
+    // Create labels for bars
+    svg.selectAll("text")
         .data(dataset)
         .enter()
         .append("text")
-        .text(function (d) {
+        .text(function(d){
             return d;
         })
-        .attr("y", function (d, i) {
-            return svgHeight - d - 2;
-        })
-        .attr("x", function (d, i) {
-            return barWidth * i;
-        })
-        .attr("fill", "#A64C38");
+        .attr("height", function (d) { return svgHeight - y(d); })
+        .attr("x", function (d, i) {return x(i) + (barWidth - 2 * barPadding)/2; })
+        .attr("y", function (d) {return y(d); })
+        .attr("text-anchor", "middle")
+        .attr("fill", "black");
+ 
 
+    // // Section to create axis
 
-        
-    // Section to create axis
+    // var x_axis = d3.axisBottom().scale(xScale);
 
-    var x_axis = d3.axisBottom().scale(xScale);
+    // var y_axis = d3.axisLeft().scale(yScale);
 
-    var y_axis = d3.axisLeft().scale(yScale);
+    // svg.append("g")
+    //     .attr("transform", "translate(50, 10)")
+    //     .call(y_axis);
 
-    svg.append("g")
-        .attr("transform", "translate(50, 10)")
-        .call(y_axis);
+    // var xAxisTranslate = svgHeight - 20;
 
-    var xAxisTranslate = svgHeight - 20;
-
-    svg.append("g")
-        .attr("transform", "translate(50, " + xAxisTranslate + ")")
-        .call(x_axis);
+    // svg.append("g")
+    //     .attr("transform", "translate(50, " + xAxisTranslate + ")")
+    //     .call(x_axis);
 
 
 
